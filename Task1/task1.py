54db4f0acfe699eeba1a5c1474a5d6c116a787f6
Aleksandr Slesarev <slessarevalex@ya.ru>
2023-02-19 17:09:25 +0300

    Решил вторую задачу

--- Task1/task1.py
+++ Task1/task1.py
@@ -5,7 +5,7 @@
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
 
-print('Введите трехзначное число', end = ' ')
+print('Введите трехзначное число: ', end = ' ')
 num = input()
 sum = int(num[0]) + int(num[1]) + int(num[2])
-print(f"{num} -> {sum} ( {num[0]} + {num[1]} + {num[2]} )")
+print(f"{num} -> {sum} ({num[0]} + {num[1]} + {num[2]})")
